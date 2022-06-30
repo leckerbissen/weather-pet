fecf929f7ce8c949221e59abfb4db7bc2bc7bca9
leckerbissen <jbazhen@gmail.com>
2022-06-30 23:36:58 +0400

    Updates

---
+++ precache-manifest.6589259ca2a4b7b8ef20c2b56e510f04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd84aa6378a1694f890",
+    "url": "/weather-pet/static/css/main.f8dd8c36.chunk.css"
+  },
+  {
+    "revision": "cfd84aa6378a1694f890",
+    "url": "/weather-pet/static/js/main.abafb2fe.chunk.js"
+  },
+  {
+    "revision": "f3b4c8a007bfbe4393f3",
+    "url": "/weather-pet/static/js/runtime~main.b3cdfa85.js"
+  },
+  {
+    "revision": "18b725d230faa24ad4dc",
+    "url": "/weather-pet/static/js/2.ccb0b3cf.chunk.js"
+  },
+  {
+    "revision": "35e3298fa08cafb94634",
+    "url": "/weather-pet/static/js/3.e131f2af.chunk.js"
+  },
+  {
+    "revision": "c4b035d46727f4af9753eb9eea2dcf59",
+    "url": "/weather-pet/static/media/morning.c4b035d4.jpg"
+  },
+  {
+    "revision": "06f80f62a3841604831e3652fa0933f3",
+    "url": "/weather-pet/static/media/evening.06f80f62.jpg"
+  },
+  {
+    "revision": "783c46ec203bea957dcd6e6782eeb28d",
+    "url": "/weather-pet/static/media/night.783c46ec.jpg"
+  },
+  {
+    "revision": "51928ff806d52f77765e430ccfc55571",
+    "url": "/weather-pet/static/media/noon.51928ff8.jpg"
+  },
+  {
+    "revision": "95b3b8f03fe525413ec33e65461e8508",
+    "url": "/weather-pet/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/weather-pet/precache-manifest.6589259ca2a4b7b8ef20c2b56e510f04.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/weather-pet/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
